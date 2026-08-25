936947236042f90ea3af865b17eb03b90b245ab2
IDjinn <42049518+IDjinn@users.noreply.github.com>
2026-08-25 12:08:07 -0300

    feat: SpeedInsights vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 import { SpaceBackground } from '@/components/space-background'
 import { Navbar } from '@/components/navbar'
 import { Footer } from '@/components/footer'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -35,6 +36,7 @@ export default function RootLayout({
         <Navbar />
         <main className="flex-1 pb-20">{children}</main>
         <Footer />
+        <SpeedInsights />
       </body>
     </html>
   )
